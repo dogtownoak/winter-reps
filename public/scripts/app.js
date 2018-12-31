0fef649ed30d60a916eb52d6ec9723f0766de574
dogtownoak <david.brandon.ford@gmail.com>
2018-12-30 17:12:48 -0800

    A. Q & A, and B. Strings answered

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,36 @@
 console.log("js up and running");
+
+// A. Q & A
+    // How do we assign a value to a variable?
+
+    // How do we change the value of a variable?
+
+    // How do we assign an existing variable to a new variable?
+
+    // Remind me, what are declare, assign, and define?
+
+    // What is pseudocoding and why shoud you do it?
+
+    // What percentage of time should be spent thinking about how you're going to solve a problem vs actually typing in code to solve it?
+
+
+// B. Strings
+
+    // 1. Create a variable called firstVariable
+
+let firstVariable = "Hello World";
+
+firstVariable = 49;
+
+let secondVariable = 49;
+
+secondVariable = "Any string";
+
+    // Answer: the value of firstVariable is 49
+
+    // 2. 
+
+let yourName = "Brandon Ford";
+
+let answer = `Hello, my name is ${yourName}`;
+
