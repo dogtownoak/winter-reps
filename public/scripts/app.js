d9ffab76b768463ae4b7e9c871ff1fe4bee4340a
dogtownoak <david.brandon.ford@gmail.com>
2018-12-30 18:29:28 -0800

    What's in your closet?

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -104,5 +104,52 @@ if (number<5) {
     }
 
 // H. What's in your closet?
+const kristynsCloset = [
+    "left shoe",
+    "cowboy boots",
+    "right sock",
+    "GA hoodie",
+    "green pants",
+    "yellow knit hat",
+    "marshmallow peeps"
+];
 
+// Thom's closet is more complicated. Check out this nested data structure!!
+const thomsCloset = [
+[
+    // These are Thom's shirts
+    "grey button-up",
+    "dark grey button-up",
+    "light blue button-up",
+    "blue button-up",
+    ],[
+    // These are Thom's pants
+    "grey jeans",
+    "jeans",
+    "PJs"
+    ],[
+    // Thom's accessories
+    "wool mittens",
+    "wool scarf",
+    "raybans"
+    ]
+];
+// 1. What's Kristyn wearing today? Using bracket notation to access items in kristynsCloset, log the sentence "Kristyn is rocking that " + the third item in Kristyn's closet + " today!" to the console.
+console.log(`Kristyn is rocking that ${kristynsCloset[2]} today!`)
+// 2. Kristyn just bought some sweet shades! Add "raybans" to her closet after "yellow knit hat".
+kristynsCloset.splice(6, 0, "raybans");
+// 3. Kristyn spilled coffee on her hat... modify this item to read "stained knit hat" instead of yellow.
+kristynsCloset[5] = "stained knit hat";
+// 4. Put together an outfit for Thom! Using bracket notation, access the first element in Thom's shirts array.
+thomsCloset[0][0];
+// 5. In the same way, access one item from Thom's pants array.
+thomsCloset[1][0];
+// 6. Access one item from Thom's accessories array.
+thomsCloset[2][0];
+// 7. Log a sentence about what Thom's wearing. Example: "Thom is looking fierce in a grey button-up, jeans and wool scarf!"
+console.log(`Thom can't wait to wear his ${thomsCloset[0][0]} with ${thomsCloset[1][0]} with ${thomsCloset[2][0]}!`);
+// 8. Get more specific about what kind of PJs Thom's wearing this winter. Modify the name of his PJ pants to Footie Pajamas.
+thomsCloset[1][2] = "Footie Pajamas";
 
+// Functions
+// A printGreeting
